a5ddef27e00b8c30cdacdc6b42aabaf0723f01bf
macia5 <nachosrlz@gmail.com>
2020-10-27 22:24:24 +0100

    reset button

--- Sim/main.py
+++ Sim/main.py
@@ -2,6 +2,7 @@ from math import fabs, sqrt
 
 import pygame
 import kilobotClass
+from button import button
 
 # inicjalizacja biblioteki
 pygame.init()
@@ -22,33 +23,47 @@ def checkPlacementCollision(array, X, Y):
             return True
     return False
 
+def reddrawWindow():
+    screen.fill((255,255,255))
+    resetButton.draw(screen, (0,0,0))
 
-
-
+resx = 800
+resy= 600
 # tworzenie ekranu
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((resx, resy))
 
 # deklaracja tablicy kilobotow
 kilobots = []
 
+#tworzenie przycisku reset
+resetButton = button((0,255,0), 0, 550, 100, 50, 'Reset')
+
 # glowna pętla
 running = True
 while running:
-
-    screen.fill((255, 255, 255))
+    reddrawWindow()
+    #screen.fill((255, 255, 255))
 
     for it in kilobots:
         position = (it.x, it.y)
         drawCircle(position, it.r, it.g, it.b)
 
     for event in pygame.event.get():
+        pos = pygame.mouse.get_pos()
 
         if event.type == pygame.QUIT:
             running = False
         if len(kilobots) <= 20:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mPosX, mPosY = pygame.mouse.get_pos()
-                if not checkPlacementCollision(kilobots, mPosX, mPosY):
-                    kilobots.append(kilobotClass.Kilobot(mPosX, mPosY, 255, 0, 0))
+                if (mPosX > 20) & (mPosY >20) & (mPosX < (resx - 70)) & (mPosY < (resy -70)):
+
+                    if not checkPlacementCollision(kilobots, mPosX, mPosY):
+                        kilobots.append(kilobotClass.Kilobot(mPosX, mPosY, 255, 0, 0))
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if resetButton.isOver(pos):
+                print('clicked reset button')
+                kilobots.clear()
 
     pygame.display.update()
