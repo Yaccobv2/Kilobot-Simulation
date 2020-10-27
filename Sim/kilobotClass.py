1cb8f4a11bedd64f5ed0af5e0832a35e92d70a12
Jakub Codogni <kubex123334@gmail.com>
2020-10-27 23:04:48 +0100

    update
    update

--- Sim/kilobotClass.py
+++ Sim/kilobotClass.py
@@ -1,15 +1,41 @@
+from cmath import sqrt
+from math import fabs
+
+import pygame
+
+
 class Kilobot:
 
-    def __init__(me, x, y, r, g, b):
-        me.x = x
-        me.y = y
-        me.r = r
-        me.g = g
-        me.b = b
+    def __init__(self, x, y, r, g, b):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.g = g
+        self.b = b
 
     x = 0
     y = 0
-    r = 255
-    g = 0
-    b = 0
+    r = 220
+    g = 220
+    b = 220
     fi = 0
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (self.r, self.g, self.b), (self.x, self.y), 20)
+
+    def move(self, x, y):
+        self.x = self.x + x
+        self.y = self.y + y
+
+    def checkSingleCollision(self, X, Y):
+            xDif = fabs(self.x - X)
+            yDif = fabs(self.y - Y)
+            Dif = sqrt(xDif ** 2 + yDif ** 2)
+            if Dif.real < 40:
+                return True
+    def checkSingleCollisionPrediction(self,self_X,self_Y X, Y):
+            xDif = fabs(self.x - X)
+            yDif = fabs(self.y - Y)
+            Dif = sqrt(xDif ** 2 + yDif ** 2)
+            if Dif.real < 40:
+                return True
--- Sim/main.py
+++ Sim/main.py
@@ -1,28 +1,22 @@
 from math import fabs, sqrt
 
 import pygame
+import random2
+
 import kilobotClass
 
 # inicjalizacja biblioteki
 pygame.init()
 
 
-# rysowanie kółek
-def drawCircle(pos, r, g, b):
-    pygame.draw.circle(screen, (r, g, b), pos, 20)
-
-
 def checkPlacementCollision(array, X, Y):
-
     for itr in array:
-        xDif = fabs(X-itr.x)
-        yDif = fabs(Y-itr.y)
-        Dif = sqrt(xDif**2+yDif**2)
+        xDif = fabs(X - itr.x)
+        yDif = fabs(Y - itr.y)
+        Dif = sqrt(xDif ** 2 + yDif ** 2)
         if Dif < 40:
             return True
     return False
-
-
 
 
 # tworzenie ekranu
@@ -38,17 +32,28 @@ while running:
     screen.fill((255, 255, 255))
 
     for it in kilobots:
-        position = (it.x, it.y)
-        drawCircle(position, it.r, it.g, it.b)
+        collision = 0
+        x = random2.randint(-1, 1)
+        y = random2.randint(-1, 1)
+
+        for it2 in kilobots:
+            if it == it2:
+                continue
+            if it.checkSingleCollision(it2.x, it2.y) and
+                collision = 1
+                break
+        if collision == 0:
+            it.move(x, y)
+        it.draw(screen)
 
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
-        if len(kilobots) <= 20:
+        if len(kilobots) < 20:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mPosX, mPosY = pygame.mouse.get_pos()
                 if not checkPlacementCollision(kilobots, mPosX, mPosY):
-                    kilobots.append(kilobotClass.Kilobot(mPosX, mPosY, 255, 0, 0))
+                    kilobots.append(kilobotClass.Kilobot(mPosX, mPosY, 210, 210, 210))
 
     pygame.display.update()
