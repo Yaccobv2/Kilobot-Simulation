b45ead475679f5d25cc6d187cc516c07c3947813
Vermiczek <49030160+Vermiczek@users.noreply.github.com>
2020-10-28 04:21:58 +0100

    uwu
    added proper movement, start and pause buttons, kilobot removal

--- Sim/button.py
+++ Sim/button.py
@@ -1,27 +1,27 @@
 import pygame
 
-#https://www.youtube.com/watch?v=4_9twnEduFA
+
+# https://www.youtube.com/watch?v=4_9twnEduFA
 class button():
-    def __init__(self, color, x, y, width, height, text=''):
+    def __init__(self, color, x, y, width, height, text='', isButton=True):
         self.color = color
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.text = text
+        self.isButton = isButton
 
-    def draw(self, win, outline=None):
+    def draw(self, win):
         # Call this method to draw the button on the screen
-        if outline:
-            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
-
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+        if self.isButton:
+            pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
 
         if self.text != '':
             font = pygame.font.SysFont('Arial', 20)
             text = font.render(self.text, 1, (0, 0, 0))
             win.blit(text, (
-            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+                self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
 
     def isOver(self, pos):
         # Pos is the mouse position or a tuple of (x,y) coordinates
